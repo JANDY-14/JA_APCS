56bf2329eb8d135c3d318299d30eb5ef5f6587af
Johnathan Anderson <ja1610010@029Labs-iMac.local>
2020-02-05 10:04:40 -0600

    thsi si some cool new stuff

---
+++ 043_Favorites/FavorioteTVShows.java
@@ -0,0 +1,7 @@
+public class FavorioteTVShows {
+    public static void main(String args[]) {
+        
+        String[] shows = { " a show 0 ", " a show 1 ", " a show 2" };
+        System.out.println(shows[0] + shows[1] + shows[2]);
+    }
+}// noice
---
+++ 043_Favorites/FavoriteMovies.java
@@ -0,0 +1,14 @@
+public class FavoriteMovies {
+
+    public static void main(String args[]) {
+
+        String[] film = new String[3];
+
+        /////////////////////////////
+        film[0] = " Ford v ferrari ";
+        film[1] = " batman ";
+        film[2] = " bee movie ";
+        ///////////////////////////////
+        System.out.println(film[0] + film[1] + film[2]);
+    }
+}// noice
